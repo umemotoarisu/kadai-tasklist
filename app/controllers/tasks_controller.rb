7d2a8b6052f628ecbeb0a80f4cee5a5e9cb6e824
umemotoarisu <alice.github318@gmail.com>
2020-08-29 10:41:09 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,57 +1,64 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in,only: [:index, :show]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
   def index
-    @tasks = Task.all
+    if logged_in?
+      @tasks = current_user.tasks
+    end
   end
-
-  def show
-  end
-
+  
   def new
     @task = Task.new
   end
-
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+    
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'タスクを投稿しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'タスクの投稿に失敗しました。'
+      render 'tasks/index'
     end
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
-
+  
   def update
+    @task = Task.find(params[:id])
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
     end
   end
-
+  
   def destroy
     @task.destroy
+    flash[:success] = 'タスクを削除しました。'
+    redirect_to root_url
+  end
+  
+   private
 
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
-  end
-  
-  private
-  # Strong Parameter
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
-  
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'tasks/create'
+  get 'tasks/destroy'
   root to: 'tasks#index'
   
   
@@ -7,7 +9,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
-  resources :tasks
+  resources :users, only: [ :new, :create]
+  resources :tasks, only: [ :new, :create, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
